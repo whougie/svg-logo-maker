01855558982354b44af4793ad85d954864787e6f
whougie <whougie.c.lo@gmail.com>
2024-04-29 22:37:13 -0500

    Adding some more node initial files

---
+++ index.js
@@ -0,0 +1,8 @@
+
+// Use inquirer to ask for color (color or hexadecimal number), shape (circle, triangle, and square), text for logo (characters)
+
+// The color is applied to the text and shape
+
+// Generate SVG to a .svg file with the output of "Generated log.svg" is printed in the command line
+
+// Write jest
